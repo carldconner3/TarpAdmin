9ce5f70e411ff7a7d9ae009c6762f4af3997b2dc
mozmani-cp <mark.ozmani@compasspointfederal.com>
2023-07-06 16:52:17 -0400

    Initial commit

---
+++ src/main/java/com/compasspoint/tarpadminportal/TarpAdminPortalApplication.java
@@ -0,0 +1,13 @@
+package com.compasspoint.tarpadminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarpAdminPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarpAdminPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compasspoint/tarpadminportal/TarpAdminPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compasspoint.tarpadminportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarpAdminPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
